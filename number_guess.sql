502fbdea674e3c1295fb487ccc0de5371b614d88
Dondomie Dungca <41827126+dondomiedungca@users.noreply.github.com>
2024-11-19 14:24:56 +0000

    fix: update commit 3

--- number_guess.sql
+++ number_guess.sql
@@ -1,6 +1,6 @@
 --
 -- PostgreSQL database dump
--- log 1 2
+-- log 1 2 3
 
 -- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
 -- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
